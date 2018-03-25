abf0c4c814c898c0971f871c1521dedb91fbd9af
Chris Waites <cwaites3@gatech.edu>
2018-03-25 02:02:48 -0700

    Minor refactor.

--- human/human/urls.py
+++ human/human/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.schemas import get_schema_view
+from rest_framework.routers import DefaultRouter
 from api import views
 
 schema_view = get_schema_view(title='Human API')
