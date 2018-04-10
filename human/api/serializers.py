4667ce1736f3b18c28eae152cae5853470f75127
Chris Waites <cwaites3@gatech.edu>
2018-04-10 16:01:09 -0400

    Fix serializers.

--- human/api/serializers.py
+++ human/api/serializers.py
@@ -11,11 +11,12 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'password')
+        fields = ('email', 'username', 'password')
 
     def create(self, validated_data):
         user = User.objects.create(
             username=validated_data['username']
+            email=validated_data['email']
         )
         user.set_password(validated_data['password'])
         user.save()
