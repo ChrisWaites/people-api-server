e3d3db465a38305e84636bae5a533fcb4e2556cc
Chris Waites <cwaites3@gatech.edu>
2018-04-14 19:08:09 -0400

    Add user to transactions.

--- api/views.py
+++ api/views.py
@@ -56,6 +56,9 @@ class TransactionViewSet(
     filter_backends = (IsOwnerFilterBackend,)
     permission_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly)
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
     def get_serializer_class(self):
         if self.action == 'create':
             return CreateTransactionSerializer
