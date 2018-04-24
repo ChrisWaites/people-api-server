8a5910eb805d3cb6dd69e2d62ed1072a50f22239
Chris Waites <cwaites3@gatech.edu>
2018-04-24 03:30:22 -0400

    Add debug.

--- server/settings.py
+++ server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#b@zxfw6#d)#hg98c2q4)a(3gciw+87r3a3x)0y+4yefpzyfhb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
