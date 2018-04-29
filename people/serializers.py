d10941696ef3dd93e789a32ffb6ba7aeb483b450
Chris Waites <cwaites3@gatech.edu>
2018-04-29 19:15:34 -0400

    Remove id from serializers where id does not have default value.

--- people/serializers.py
+++ people/serializers.py
@@ -33,7 +33,7 @@ class DepositSerializer(serializers.ModelSerializer):
 class CreateDepositSerializer(serializers.ModelSerializer):
     class Meta:
         model = Deposit
-        fields = ('id', 'stripeToken', 'amount')
+        fields = ('stripeToken', 'amount')
 
 
 class TransferSerializer(serializers.ModelSerializer):
@@ -45,7 +45,7 @@ class TransferSerializer(serializers.ModelSerializer):
 class CreateTransferSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transfer
-        fields = ('id', 'amount')
+        fields = ('amount',)
 
 
 class AttributeSerializer(serializers.ModelSerializer):
